706aca687238ccabdfac73dc1632e4d19d5597f6
Toure2019 <51404116+Toure2019@users.noreply.github.com>
2019-11-20 17:47:38 +0100

    deploiement de l'application sur le net avec heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+const server = express();
+server.use(express.static(__dirname));
+
+server.get('/bonjour', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'index.html'));
+});
+
+server.listen(port);
+console.log('Serveur en écoute sur le port:', port);
